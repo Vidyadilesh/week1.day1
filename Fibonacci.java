0c94f51c88c42bc8eb2982e5845bbbe90b75807b
Vidyadilesh <98165408+Vidyadilesh@users.noreply.github.com>
2022-01-27 14:50:04 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		int firstnum = 0 ;
+		int secnum = 1;
+		int range = 10;
+		
+		System.out.println("First Number : " +firstnum);
+		for (int i=0;i<range;i++) {
+			int sum = firstnum + secnum;
+			firstnum = secnum;
+			secnum = sum;
+			System.out.println(sum);
+		}
+
+	}
+
+}
---
+++ OddNumber.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class OddNumber {
+
+	public static void main(String[] args) {
+		System.out.println("Printing Odd Numbers from 1 to 10");
+		int a = 10;
+		for (int i=1;i<a;i++) {
+			if(i%2==1) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
